576b1ca0e56074ab55f68b932206f6ed621e1f57
Travis Keshemberg <45918944+tlkcfa@users.noreply.github.com>
2019-03-24 11:58:05 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a special metrix object that can cache its inverse
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a special “matrix”, 
+## this is a list containing a function that will
+## 1. set the value of the matrix, 2. get the value of the matrix 
+## 3. set the value of the inverse 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special maxtrix returned above
+## if the inverse was calculated then this will retreieve the inverse 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if (!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
+
+## X <- matrix(c(1,2,3,4),2,2)
+## > X1 <- makeCacheMatrix(X)
+## > cacheSolve(X1)
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
+
